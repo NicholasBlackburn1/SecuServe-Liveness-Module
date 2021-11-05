f251545504821668ff6d179a7e4446c84649654b
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-11-05 09:13:08 -0400

    added uwu

--- SecuServe-Livenes-Module/__main__.py
+++ SecuServe-Livenes-Module/__main__.py
@@ -4,6 +4,17 @@ this is the main class for the liveeness dector
 
 from utils import consoleLog
 import zmq
+from datetime import datetime
+from utility.video_utils import VideoUtils
+from face_det.TDDFA import TDDFA
+from face_det.FaceBoxes import FaceBoxes
+from face_detector import FaceDetector
+from utility import main as mn
+import cv2
+import yaml
+import numpy as np
+import tensorflow as tf
+
 
 def main():
     consoleLog.Warning("Starting Zmq....")
@@ -11,8 +22,13 @@ def main():
     context = zmq.Context()
     sender = context.socket(zmq.PUB)
     sender.bind("tcp://" + "127.0.0.1:5000")
+
+    sender.send_string("LIVENESS")
+    sender.send_json({'status':"Starting",'alive':False,'time':str(datetime.now)})
     
     consoleLog.PipeLine_Ok("Started Zmq...")
+    mn()
+
     
 
 
