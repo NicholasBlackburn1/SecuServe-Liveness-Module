997c77b5b01e88dd8a807caf7d5d22c2df73ed28
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-12-21 09:05:01 -0500

    working on eye movement detection not just blinking

--- SecuServe-Livenes-Module/pipeline/LiveDetection.py
+++ SecuServe-Livenes-Module/pipeline/LiveDetection.py
@@ -24,6 +24,9 @@ class LiveDetection(object):
     EYE_AR_CONSEC_FRAMES = 2.0
     TOTAL = 0
 
+    left_counter = 0 
+    right_counter = 0
+
     (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
@@ -151,4 +154,58 @@ class LiveDetection(object):
                 self.COUNTER = 0
 
 
-    
+    def eyeThresholding(self,value ):  # function to threshold and give either left or right
+        
+        if (value<=54):   #check the parameter is less than equal or greater than range to 
+            left_counter=self.left_counter+1		#increment left counter 
+
+            if (left_counter>self.th_value):  #if left counter is greater than threshold value 
+                consoleLog.info('RIGHT EYE')
+
+            if(self.right_counter>self.th_value):
+                consoleLog.info('LEFT EYE')
+                self.right_counter=0
+
+    #* eye detection and pos location
+    def eyePosDetection(self,frame,ret):
+        
+        cv2.line(frame, (320,0), (320,480), (0,200,0), 2)
+        cv2.line(frame, (0,200), (640,200), (0,200,0), 2)
+        
+        if ret==True:
+            col=frame
+            
+            frame = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+
+            pupilFrame=frame
+            clahe=frame
+            blur=frame
+            edges=frame
+
+            eyes = cv2.CascadeClassifier('haarcascade_eye.xml')
+            detected = eyes.detectMultiScale(frame, 1.3, 5)
+
+            for (x,y,w,h) in detected: #similar to face detection but for eyes
+
+                cv2.rectangle(frame, (x,y), ((x+w),(y+h)), (0,0,255),1)	 #draw rectangle around eyes
+                cv2.line(frame, (x,y), ((x+w,y+h)), (0,0,255),1)   #draw cross
+                cv2.line(frame, (x+w,y), ((x,y+h)), (0,0,255),1)
+
+                pupilFrame = cv2.equalizeHist(frame[y+(h*.25):(y+h), x:(x+w)]) #using histogram equalization of better image. 
+                
+                cl1 = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8)) #set grid size
+                clahe = cl1.apply(pupilFrame)  #clahe
+                blur = cv2.medianBlur(clahe, 7)  #median blur
+                circles = cv2.HoughCircles(blur ,cv2.cv.CV_HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=7,maxRadius=21) #houghcircles
+
+                if circles is not None: #if atleast 1 is detected
+                    circles = np.round(circles[0, :]).astype("int") #change float to integer
+
+                    consoleLog.info( 'integer for eye pos ',circles)
+                    for (x,y,r) in circles:
+                        cv2.circle(pupilFrame, (x, y), r, (0, 255, 255), 2)
+                        cv2.rectangle(pupilFrame, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+                        #set thresholds
+                        thresholding(x)
+                        
+        
