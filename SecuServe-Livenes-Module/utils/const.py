eafa0f1a149b545aaf0c0049a30c10e41398d1ac
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-03-15 14:16:49 -0400

    working on setting up reply back in the sockets

--- SecuServe-Livenes-Module/utils/const.py
+++ SecuServe-Livenes-Module/utils/const.py
@@ -3,7 +3,7 @@ this is where the const go for the module
 """
 from  configparser import ConfigParser
 
-zmq_recv = "tcp://" + "127.0.0.1:5000"
+zmq_recv = "tcp://" + "127.0.0.1:5001"
 zmq_sender = "tcp://" + "127.0.0.1:5002"
 
 
